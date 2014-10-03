5be5ad56b4833d14eaffc004992e0db6fce62196
TubalLara <tublargar@gmail.com>
2014-10-03 10:43:16 +0200

    con dom

--- WebApplication2/scripts/nodos.js
+++ WebApplication2/scripts/nodos.js
@@ -7,3 +7,26 @@
     document.getElementById("hijo1").appendChild(item);
 
 };
+
+var borrarNodo = function (id) {
+    var nodo = document.getElementById(id);
+    var padre = nodo.parentNode;
+
+    padre.removeChild(nodo);
+}
+
+var cambiarColor = function (id) {
+    var nodo = document.getElementById(id);
+    nodo.className = "rosita";
+
+}
+
+//cuando tengamos una longitud que se recalcule utilizar mejor un while que un for
+var todos = function () {
+    var p = document.getElementById("padre");
+
+    while (p.childNodes.length>0) {
+
+        p.removeChild(p.childNodes[0]);
+    }
+}
