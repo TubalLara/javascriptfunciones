ca5f65975a79abed5c5de82ca790380f2b8b8132
TubalLara <tublargar@gmail.com>
2014-10-03 09:49:56 +0200

    con nom

---
+++ WebApplication2/scripts/nodos.js
@@ -0,0 +1,9 @@
+﻿var addLista = function () {
+
+    var item = document.createElement("li");
+    var texto = document.createTextNode("Me han creado nuevo");
+    item.appendChild(texto);
+
+    document.getElementById("hijo1").appendChild(item);
+
+};
